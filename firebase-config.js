eac12a11d7ebae084e99953db9393aab3e5e471a
Guelg4 <pmiguelg123@gmail.com>
2026-06-26 23:21:34 -0300

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -2,12 +2,12 @@
    firebase-config.js — Álbum Copinha
    ========================================================= */
 const firebaseConfig = {
-  apiKey:            "AIzaSyCqam6xaSiYE2ax8F7uBZHoRNr_m0XPGNA",
+  apiKey:            "AIzaSyDhi3PAO9FHq_IZ8tM1HpnK7R4lGt3mVpQ",
   authDomain:        "album-copinha.firebaseapp.com",
   projectId:         "album-copinha",
   storageBucket:     "album-copinha.firebasestorage.app",
   messagingSenderId: "791583378158",
-  appId:             "1:791583378158:web:be36be42e2eadcfbcb01ff"
+  appId:             "1:791583378158:web:ca1dfe29f46e17cfcb01ff"
 };
 
 firebase.initializeApp(firebaseConfig);
